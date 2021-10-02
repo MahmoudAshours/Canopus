1ad3cf7b20444e477b9818da6906209141077262
Mahmoud Ashour <mashour365@gmail.com>
2021-10-02 15:38:01 +0200

    Added Quotes class

---
+++ lib/Constants/quotes.dart
@@ -0,0 +1,42 @@
+class SunQuotes {
+  static List<Map<String, String>> famousQuotes = [
+    {
+      'quote': 'Keep your face to the sun and you will never see the shadows.',
+      'by': 'Helen Keller'
+    },
+    {
+      'quote': 'Even for me life had its gleams of sunshine',
+      'by': 'Charlotte Brontë, Jane Eyre'
+    },
+    {
+      'quote': 'What sunshine is to flowers, smiles are to humanity.',
+      'by': 'Joseph Addison'
+    },
+    {
+      'quote': 'Ô, Sunlight! The most precious gold to be found on Earth.',
+      'by': 'Roman Payne'
+    },
+    {
+      'quote':
+          'The sun,--the bright sun, that brings back, not light alone, but new life, and hope.',
+      'by': 'Charles Dickens, Oliver Twist'
+    },
+    {
+      'quote': 'A flower cannot blossom without sunshine.',
+      'by': 'Max Muller',
+    },
+    {
+      'quote': 'Be the sunshine on a cloudy day.',
+      'by': 'Christina Casino',
+    },
+    {
+      'quote': 'The future is green energy, sustainability, renewable energy.',
+      'by': 'Arnold Schwarzenegger'
+    },
+    {
+      'quote':
+          "I've been very passionate about renewable energy for many years, particularly solar energy and its capacity to bring abundant clean, sustainable energy to millions around the globe.",
+      'by': 'Richard Branson'
+    },
+  ];
+}
