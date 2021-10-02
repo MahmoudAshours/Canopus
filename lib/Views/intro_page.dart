656466fafb27c10ad1bb0e446fdacbece80c95d9
Mahmoud Ashour <mashour365@gmail.com>
2021-10-02 15:44:15 +0200

    Added intro page view

---
+++ lib/Views/intro_page.dart
@@ -0,0 +1,25 @@
+import 'package:canopus/ViewModel/intro_views.dart';
+import 'package:canopus/Views/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroViewsFlutter(
+        IntroViewsConstants().page,
+        onTapDoneButton: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (_) => const HomeScreen())),
+        showSkipButton: true,
+        skipText: const Text('Skip', style: TextStyle(color: Colors.black)),
+        pageButtonTextStyles: const TextStyle(
+          color: Colors.black,
+          fontSize: 18.0,
+        ),
+      ),
+    );
+  }
+}
