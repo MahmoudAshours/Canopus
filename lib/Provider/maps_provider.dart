d5b106255079d9495ab891c82e34c1ddf799c204
Mahmoud Ashour <mashour365@gmail.com>
2021-10-02 19:05:55 +0200

    Added maps provider

---
+++ lib/Provider/maps_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapsProvider extends ChangeNotifier {
+  LatLng _latLng = const LatLng(45.521563, -122.677433);
+  LatLng get getLatLng => _latLng;
+  set latLng(LatLng newLatLng) {
+    _latLng = newLatLng;
+    notifyListeners();
+  }
+}
