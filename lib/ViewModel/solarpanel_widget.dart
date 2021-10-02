624bc0eab5d508e1d4c00fd957650f771a0a53c2
Mahmoud Ashour <mashour365@gmail.com>
2021-10-02 15:39:58 +0200

    Added Solar panel 3D cube screen

---
+++ lib/ViewModel/solarpanel_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cube/flutter_cube.dart';
+
+class SolarPanel3D extends StatefulWidget {
+  const SolarPanel3D({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<SolarPanel3D>
+    with SingleTickerProviderStateMixin {
+  late Scene _scene;
+  Object? _panel;
+  late AnimationController _controller;
+
+  void _onSceneCreated(Scene scene) {
+    _scene = scene;
+    _scene.camera.position.z = 2;
+    _panel = Object(
+        backfaceCulling: false,
+        fileName: 'assets/images/solar_panel/objSolar.obj');
+    _scene.world.add(_panel!);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 30000), vsync: this)
+      ..addListener(() {
+        if (_panel != null) {
+          _panel!.rotation.y = _controller.value * 360;
+          _panel!.updateTransform();
+          _scene.update();
+        }
+      })
+      ..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Cube(onSceneCreated: _onSceneCreated);
+}
