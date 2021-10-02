e8d05ae88fc0334e7f0fa336c78dea68c8e69d17
Mahmoud Ashour <mashour365@gmail.com>
2021-10-02 19:05:47 +0200

    Added date provider

---
+++ lib/Provider/date_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+
+class DateProvider extends ChangeNotifier {
+  String _startDate = "";
+  String _endDate = "";
+
+  String get getStartDate => _startDate.replaceAll('/', '');
+  String get getEndDate => _endDate.replaceAll('/', '');
+
+  set startDate(String startDate) {
+    _startDate = startDate;
+    notifyListeners();
+  }
+
+  set endDate(String endDate) {
+    _endDate = endDate;
+    notifyListeners();
+  }
+}
