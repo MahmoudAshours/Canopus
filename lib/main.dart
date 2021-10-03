31c41abb376a3405cd2b6cd7f85d422eb1136e21
Mahmoud Ashour <mashour365@gmail.com>
2021-10-03 03:27:39 +0200

    Added shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:canopus/Themes/canopus_theme.dart';
+import 'package:canopus/Views/home_screen.dart';
 import 'package:canopus/Views/intro_page.dart';
+import 'package:canopus/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    CanopusProviders(
+      child: MaterialApp(
+        title: 'Canopus',
+        theme: theme,
+        home: await checkIntro() ? const IntroPage() : const HomeScreen(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Canopus',
-      theme: theme,
-      home: const IntroPage(),
-    );
+/// Check if it's the user first time in the app.
+Future<bool> checkIntro() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  try {
+    return prefs.getBool('intro')!;
+  } catch (e) {
+    prefs.setBool('intro', false);
+    return true;
   }
 }
