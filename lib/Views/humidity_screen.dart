703a771232eeeb5df3098daa0bedbafaa942e695
Mahmoud Ashour <mashour365@gmail.com>
2021-10-03 03:29:38 +0200

    Code refactor

--- lib/Views/humidity_screen.dart
+++ lib/Views/humidity_screen.dart
@@ -1,17 +1,26 @@
+import 'dart:math';
+
+import 'package:canopus/Provider/maps_provider.dart';
 import 'package:canopus/Views/graph_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 class HumidityScreen extends StatelessWidget {
   final value;
   HumidityScreen({Key? key, this.value}) : super(key: key);
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  double roundDouble(double value, int places) {
+    num mod = pow(10.0, places);
+    return ((value * mod).round().toDouble() / mod);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final _mapsProvider = Provider.of<MapsProvider>(context);
     return SafeArea(
       child: Column(
         children: [
@@ -20,7 +29,9 @@ class HumidityScreen extends StatelessWidget {
             child: Text(
               'According to your location : ',
               style: TextStyle(
-                  color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold),
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
             ),
           ),
           SizedBox(
@@ -37,7 +48,7 @@ class HumidityScreen extends StatelessWidget {
               tiltGesturesEnabled: false,
               rotateGesturesEnabled: false,
               initialCameraPosition:
-                  CameraPosition(target: _center, zoom: 30.0),
+                  CameraPosition(target: _mapsProvider.getLatLng, zoom: 30.0),
             ),
           ),
           Padding(
@@ -50,32 +61,19 @@ class HumidityScreen extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Container(
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Icon(Icons.ac_unit, color: Colors.white),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: const LinearGradient(
-                            colors: [Color(0xff00AAFF), Color(0xff008BFD)],
-                          ),
-                        ),
-                      ),
-                    ),
                     const SizedBox(height: 10),
                     const Text(
                       'Average humidity percentage',
-                      style: TextStyle(color: Colors.white, fontFamily: 'asd'),
+                      style: TextStyle(color: Colors.white),
                     ),
                     const SizedBox(height: 10),
                     Center(
                       child: Text(
-                        '${value['properties']['parameter']['RH2M'].keys.toList().map((m) => value['properties']['parameter']['RH2M'][m]).reduce((a, b) => a + b) / value['properties']['parameter']['RH2M'].keys.toList().length}',
-                        style: const TextStyle(color: Colors.white),
+                        '${value['properties']['parameter']['RH2M'].keys.toList().map((m) => value['properties']['parameter']['RH2M'][m]).reduce((a, b) => roundDouble(a + b / value['properties']['parameter']['RH2M'].keys.toList().length, 2))} m/s',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 25),
                       ),
                     ),
                   ],
@@ -96,33 +94,19 @@ class HumidityScreen extends StatelessWidget {
                       color: const Color(0xff262A35),
                       borderRadius: BorderRadius.circular(30)),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          child: const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.ac_unit, color: Colors.white),
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            gradient: const LinearGradient(
-                              colors: [Color(0xff00AAFF), Color(0xff008BFD)],
-                            ),
-                          ),
-                        ),
-                      ),
                       const SizedBox(height: 10),
                       const Text(
                         'Maximum humidity percentage',
-                        style:
-                            TextStyle(color: Colors.white, fontFamily: 'asd'),
+                        style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                       const SizedBox(height: 10),
                       Center(
                         child: Text(
                           '${value['properties']['parameter']['RH2M'].keys.toList().map((m) => value['properties']['parameter']['RH2M'][m]).reduce((a, b) => a > b ? a : b)} m/s',
-                          style: const TextStyle(color: Colors.white),
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 25),
                         ),
                       ),
                     ],
@@ -131,25 +115,11 @@ class HumidityScreen extends StatelessWidget {
                 Container(
                   width: 200,
                   decoration: BoxDecoration(
-                      color: Colors.orange,
+                      color: const Color(0xff262A35),
                       borderRadius: BorderRadius.circular(30)),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          child: const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.ac_unit, color: Colors.white),
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            gradient: const LinearGradient(
-                              colors: [Color(0xff00AAFF), Color(0xff0F0413)],
-                            ),
-                          ),
-                        ),
-                      ),
                       const SizedBox(height: 10),
                       const Text(
                         'Minimum humidity percentage',
@@ -159,7 +129,8 @@ class HumidityScreen extends StatelessWidget {
                       Center(
                         child: Text(
                           '${value['properties']['parameter']['RH2M'].keys.toList().map((m) => value['properties']['parameter']['RH2M'][m]).reduce((a, b) => a < b ? a : b)} m/s',
-                          style: const TextStyle(color: Colors.red),
+                          style: const TextStyle(
+                              fontSize: 26, color: Colors.white),
                         ),
                       ),
                     ],
@@ -173,22 +144,18 @@ class HumidityScreen extends StatelessWidget {
             child: ElevatedButton(
               onPressed: () => Navigator.of(context).push(
                 MaterialPageRoute(
-                    builder: (_) => GraphScreen(
-                        name: "Humidity percentage",
-                        value: value['properties']['parameter']['RH2M'])),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.orange),
-                fixedSize: MaterialStateProperty.all(
-                  Size(MediaQuery.of(context).size.width, 30),
+                  builder: (_) => GraphScreen(
+                      name: "Humidity percentage",
+                      value: value['properties']['parameter']['RH2M']),
                 ),
               ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  fixedSize: MaterialStateProperty.all(
+                      Size(MediaQuery.of(context).size.width, 30))),
               child: const Text(
                 'Check graphs!',
-                style: TextStyle(
-                    fontFamily: 'wave',
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
             ),
           )
